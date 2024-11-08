d9e263ff32b86e6fcf94951c93e0dbc2da5a2710
Holm-Slice <taylorholm92@gmail.com>
2024-11-08 11:55:30 -0600

    changed asset path on navbar img and scroller imgs. removed /src/asset, changed to /asset

--- src/components/HorizontalScroller/HorizontalScroller.jsx
+++ src/components/HorizontalScroller/HorizontalScroller.jsx
@@ -5,17 +5,17 @@ function HorizontalScroller() {
     <div className="scroll-container">
       <div className="scroll-content">
         <img
-          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
