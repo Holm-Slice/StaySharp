9b314a8bb12f20bf51ba41517f9c1eea72550210
Holm-Slice <taylorholm92@gmail.com>
2024-11-08 12:25:56 -0600

    change all hotizontal scroller relative paths to src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png and src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Indigo.png

--- src/components/HorizontalScroller/HorizontalScroller.jsx
+++ src/components/HorizontalScroller/HorizontalScroller.jsx
@@ -5,196 +5,197 @@ function HorizontalScroller() {
     <div className="scroll-container">
       <div className="scroll-content">
         <img
-          src="/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Indigo.png"
+          src="src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Indigo.png"
+          alt="stay-sharp-icon"
           alt="stay-sharp-wordmark"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
 
         {/* color changes to purple (pink) */}
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Purple.png"
+          src="src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Purple.png"
           alt="stay-sharp-wordmark"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         {/* color changes to teal */}
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Teal.png"
+          src="src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Teal.png"
           alt="stay-sharp-wordmark"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         {/* starting color scheme completely over */}
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Indigo.png"
+          src="src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Indigo.png"
           alt="stay-sharp-wordmark"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Indigo.png"
           alt="stay-sharp-icon"
           className="scroll-item"
         />
 
         {/* color changes to purple (pink) */}
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Purple.png"
+          src="src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Purple.png"
           alt="stay-sharp-wordmark"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Purple.png"
           className="scroll-item"
         />
         {/* color changes to teal */}
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Teal.png"
+          src="src/assets/imgs/wordmark-PNG/Stay Sharp Wordmark Teal.png"
           alt="stay-sharp-wordmark"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
         <img
-          src="public/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
+          src="src/assets/imgs/icon-PNG/Stay Sharp Icon Teal.png"
           className="scroll-item"
         />
       </div>
