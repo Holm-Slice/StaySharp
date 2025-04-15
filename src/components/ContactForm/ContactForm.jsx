94093d91697433a29591513bdef420643e186883
Holm-Slice <taylorholm92@gmail.com>
2025-04-15 10:04:39 -0500

    contact form re work

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
-import "./ContactForm.css"; // Import the CSS file
 
 function ContactForm() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
+    phone: "",
     message: "",
   });
 
@@ -35,67 +35,110 @@ function ContactForm() {
     setFormData({
       name: "",
       email: "",
+      phone: "",
       message: "",
     });
   };
 
   return (
-    <div id="contact" className="contact-form-container">
-      {/* Background Image */}
-      <img
-        src="/assets/Images/logo-circle-10x.png"
-        alt="Background"
-        className="background-image"
-      />
+    <div
+      id="contact"
+      className="flex flex-col justify-center items-center p-4 m-4 md:p-8 md:m-8 overflow-hidden"
+    >
+      <main className="bg-white border-2 border-ss_purple w-2/3 max-w-2/3 p-4 md:p-8 shadow-[8px_8px_0px_#453393] hover:transition-transform hover:scale-110 hover:duration-[2000ms] duration-[3000ms] cursor-pointer gap-4 overflow-hidden">
+        <h1 className="font-title font-bold text-2xl md:text-3xl text-center mb-6 text-ss_purple">
+          Let's Connect
+        </h1>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          {/* Name, Email, and Phone Fields */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Name Field */}
+            <div className="form-group">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:shadow-[0_0_10px_#453393] focus:border-ss_purple focus:ring-ss_purple focus:outline-none sm:text-sm h-11 pl-2"
+              />
+            </div>
 
-      {/* Title */}
-      <div className="title-container">
-        <h1>Let's Connect</h1>
-      </div>
+            {/* Email Field */}
+            <div className="form-group">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:shadow-[0_0_10px_#453393] focus:border-ss_purple focus:ring-ss_purple focus:outline-none sm:text-sm h-11 pl-2"
+              />
+            </div>
+          </div>
 
-      {/* Form */}
-      <form className="contact-form" onSubmit={handleSubmit}>
-        {/* Name Field */}
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          {/* Phone Field */}
+          <div className="form-group">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Phone:
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              required
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:shadow-[0_0_10px_#453393] focus:border-ss_purple focus:ring-ss_purple focus:outline-none sm:text-sm h-11 pl-2"
+            />
+          </div>
 
-        {/* Email Field */}
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          {/* Message Field */}
+          <div className="form-group">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Message:
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:shadow-[0_0_10px_#453393] focus:border-ss_purple focus:ring-ss_purple focus:outline-none sm:text-sm h-24 pl-2 pt-2"
+            ></textarea>
+          </div>
 
-        {/* Message Field */}
-        <div className="form-group">
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-
-        {/* Submit Button */}
-        <button type="submit">Send</button>
-      </form>
+          {/* Submit Button */}
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-ss_purple text-white uppercase py-2 px-4 md:py-3 md:px-6 hover:bg-white hover:text-ss_purple transition-colors duration-[1300ms] border-4 border-ss_purple"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
